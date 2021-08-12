edea62f0dc15fe9822536572c5a11462de786471
Ahsan181a <ahsanbondhan181@gmail.com>
2021-08-12 09:38:47 +0600

    this is a firebase auth

---
+++ public/js/firebase-conf.js
@@ -0,0 +1,15 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyALLzrT4iyknjoOGcrwj62ROUynvrDTzQ8",
+    authDomain: "codecreator.firebaseapp.com",
+    projectId: "codecreator",
+    storageBucket: "codecreator.appspot.com",
+    messagingSenderId: "594704298144",
+    appId: "1:594704298144:web:c919de898897351b5899c5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // facebook
+  var facebookProvider = new firebase.auth.FacebookAuthProvider();
+
